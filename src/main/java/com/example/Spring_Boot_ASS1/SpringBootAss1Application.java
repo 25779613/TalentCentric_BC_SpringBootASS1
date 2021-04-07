a0bb376373d1a60ced944fc8979f3a80e131e51c
25779613 <duk.sal01@gmail.com>
2021-04-07 22:40:32 +0200

    The first spring boot assignment

---
+++ src/main/java/com/example/Spring_Boot_ASS1/SpringBootAss1Application.java
@@ -0,0 +1,58 @@
+package com.example.Spring_Boot_ASS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Controller
+//@RestController
+public class SpringBootAss1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAss1Application.class, args);
+
+
+    }
+
+    @GetMapping("/Welcome")
+
+    public String Welcome(Model model) {
+
+
+
+        return String.format("Nice to meet you ready to start?");
+
+    }
+
+    @GetMapping("/welcome")
+    public String showGreeting(Model model)
+    {
+        String[] greet = {"Nice to meet you,36++ ready to start?","this is unnecessary","but you gotta learn somehow"};
+        model.addAttribute("greeting", greet);
+
+
+        return "welcome";
+    }
+
+    @GetMapping("/users")
+
+    public String goUsers(Model model) {
+        String [] members = {"joe", "sal", "king kong"};
+        model.addAttribute("newUsers", members);
+
+        return "users";
+    }
+
+    @GetMapping("/meNombre")
+    public String name(Model model)
+    {
+        String jeff = "myNameJEFF";
+        model.addAttribute("meNombre", jeff);
+        return "meNombre";
+
+    }
+}
---
+++ src/test/java/com/example/Spring_Boot_ASS1/SpringBootAss1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_ASS1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAss1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
